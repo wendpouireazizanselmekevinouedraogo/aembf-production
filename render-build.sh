c0dae6dff4e1fb15181116d78b57e826b278e653
OUEDRAOGO Aziz <wendpouireazizanselmekevinoued@gmail.com>
2026-08-02 16:16:02 +0100

    Correction installation composer dans le script de build

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit
 
-composer install --no-dev --optimize-autoloader
+echo "Téléchargement et installation de Composer..."
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+echo "Installation des dépendances PHP avec Composer..."
+php composer.phar install --no-dev --optimize-autoloader
+
+echo "Nettoyage et mise en cache de Laravel..."
+php composer.phar dump-autoload
 php artisan config:clear
 php artisan config:cache
 php artisan route:cache
